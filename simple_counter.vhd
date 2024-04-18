ff791961e57ad4f12992a0f9b8c3c0de88e61e00
Jakub230332 <166016097+Jakub230332@users.noreply.github.com>
2024-04-18 13:38:58 +0200

    Update simple_counter.vhd

--- simple_counter.vhd
+++ simple_counter.vhd
@@ -1,13 +1,46 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/07/2024 01:07:40 PM
+-- Design Name: 
+-- Module Name: simple_counter - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_unsigned.all;
 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
 entity simple_counter is
-    Generic ( N : integer := 16);
+    Generic ( N : integer := 4);
     Port ( clk : in STD_LOGIC;
-           rst : in STD_LOGIC;
+           dalsi : in STD_LOGIC;
+           prev : in std_logic;
+           back : in std_logic;
+           forw : in std_logic;
            en : in STD_LOGIC;
-           count : out STD_LOGIC_VECTOR (16 downto 0));
+           count : out STD_LOGIC_VECTOR (N-1 downto 0));
 end simple_counter;
 
 architecture Behavioral of simple_counter is
@@ -19,10 +52,14 @@ begin
     process (clk)
     begin
        if rising_edge(clk) then
-          if rst='1' then
+          if dalsi='1' or prev = '1' then
              sig_count <= (others => '0');
           elsif en='1' then
              sig_count <= sig_count + 1;
+          elsif forw = '1' then
+             sig_count <= sig_count + 4;
+          elsif back = '1' then
+             sig_count <= sig_count - 4;
           end if;
        end if;
     end process;
