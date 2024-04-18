2a13449c0a26aa89ac2bdb81eb5658e8654140da
CharvatekVit <166016130+CharvatekVit@users.noreply.github.com>
2024-04-18 14:09:12 +0200

    Update bin2seven.vhd

--- bin2seven.vhd
+++ bin2seven.vhd
@@ -1,36 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/22/2024 01:11:44 PM
--- Design Name: 
--- Module Name: bin2seg - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_unsigned.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity bin2seven is
     Port ( bin : in STD_LOGIC_VECTOR (1 downto 0);
