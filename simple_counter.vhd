ec3ebfb228cf9d7f8d1d740e96a41335a982f59b
CharvatekVit <166016130+CharvatekVit@users.noreply.github.com>
2024-05-02 12:10:11 +0200

    Update simple_counter.vhd

--- simple_counter.vhd
+++ simple_counter.vhd
@@ -1,36 +1,8 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/07/2024 01:07:40 PM
--- Design Name: 
--- Module Name: simple_counter - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_unsigned.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity simple_counter is
     Generic ( N : integer := 4);
